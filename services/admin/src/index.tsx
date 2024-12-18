b5d72b1fc6605746c50fd98154c77067ce0a7b97
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-12-18 18:05:14 +0400

    delete unused imports

--- services/admin/src/index.tsx
+++ services/admin/src/index.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
-import {  } from '@packages/build_config';
-import { } from '@packages/shared';
 import Admin from './pages/Admin';
 
 const router = createBrowserRouter([
